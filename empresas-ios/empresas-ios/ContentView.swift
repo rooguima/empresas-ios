123cc59735d43d2011efdcb8edab90c5c2421a19
Rodrigo <rooguima@icloud.com>
2020-10-05 18:33:08 -0300

    Initial code

---
+++ empresas-ios/empresas-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  empresas-ios
+//
+//  Created by Rodrigo Guimarães on 05/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
